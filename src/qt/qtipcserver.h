df928ce18226ad8d096be574bb1209540da4afb4
MrBoat1337 <mrboat1337@gmail.com>
2017-07-15 21:50:58 +1000

    Initiate OLIT

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define OliTwistCoin-qt message queue name
+#define BITCOINURI_QUEUE_NAME "OliTwistCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
